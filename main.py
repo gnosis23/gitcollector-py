9c72a8b7e1a328fe6cafd63821ad4119abb0ecba
BOHAO WANG <bj050323@gmail.com>
2025-12-14 11:08:33 +0800

    feat: 优化输出

--- main.py
+++ main.py
@@ -60,7 +60,7 @@ def main():
         count_by_weekday[nth] += day.count
     for i in range(7):
         percent = count_by_weekday[i] / total_commit * 100
-        print(f"  {weekdays[i]:<3} {count_by_weekday[i]:>4} times ({percent:.2f}%)")
+        print(f"  {weekdays[i]:<3} {count_by_weekday[i]:>5} times ({percent:.2f}%)")
 
     print_blank()
 
@@ -77,7 +77,7 @@ def main():
             total_hours += 1
     for i in range(24):
         percent = count_by_hours[i] / total_hours * 100 if total_hours else 0
-        print(f"  {i:>2} clock   {count_by_hours[i]:>2} days ({percent:.2f}%)")
+        print(f"  {i:>2} clock   {count_by_hours[i]:>4} days ({percent:.2f}%)")
 
     print_blank()
 
