befcfa55cbe40cf5580e8a7946814103e49cfb2d
BOHAO WANG <bj050323@gmail.com>
2025-12-17 22:57:52 +0800

    feat: init

--- collector/__init__.py
+++ collector/__init__.py
@@ -1,1 +1,12 @@
 # Auto
+
+from .commit import count_commits, get_daily_commit_counts
+from .util import get_weekday
+
+__all__ = [
+    "count_commits",
+    "get_daily_commit_counts",
+    "get_weekday",
+    "DailyCommitCount",
+    "DailyCommitHours",
+]
